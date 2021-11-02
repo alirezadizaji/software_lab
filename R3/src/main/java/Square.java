bb7e757d40977fae1b0039151227393d22e6c2b7
Hamidreza <hamidreza004@gmail.com>
2021-11-03 00:28:31 +0330

    Implement body of square functions

--- R3/src/main/java/Square.java
+++ R3/src/main/java/Square.java
@@ -1,17 +1,23 @@
 public class Square extends Shape implements areaCalculation {
     private int edge;
     public Square(int edge) throws Exception{
+        if (edge <= 0)
+            throw new Exception("Negative Square");
+        this.edge = edge;
     }
 
     @Override
     public int calculateArea() {
-        return 0;
+        return this.edge * this.edge;
     }
 
     public void setEdge(int edge) throws Exception{
+        if (edge <= 0)
+            throw new Exception("Negative Square");
+        this.edge = edge;
     }
 
     public int getEdge() {
-        return 0;
+        return this.edge;
     }
 }
