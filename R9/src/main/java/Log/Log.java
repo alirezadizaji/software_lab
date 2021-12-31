3669a051cd7c823e593eeb29c819fdfc49d6c446
Hamidreza <hamidreza004@gmail.com>
2022-01-01 02:07:16 +0330

    Add logOn logic to Log

--- R9/src/main/java/Log/Log.java
+++ R9/src/main/java/Log/Log.java
@@ -4,7 +4,16 @@ package Log;
  * Created by mohammad hosein on 6/29/2015.
  */
 public class Log {
+    private static boolean logOn = false;
+    public void turnOn() {
+        logOn = true;
+    }
+    public void turnOff() {
+        logOn = false;
+    }
     public static void print(String s)
     {
+        if (logOn)
+            System.out.println(s);
     }
 }
